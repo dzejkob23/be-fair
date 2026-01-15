db379be516175c77f4898319d6b08f90e58394fc
Jakub Zíka <info@jakubzika.dev>
2026-01-15 14:53:30 +0100

    Initial commit

---
+++ composeApp/src/commonTest/kotlin/dev/jakubzika/befair/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package dev.jakubzika.befair
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ shared/src/commonMain/kotlin/dev/jakubzika/befair/Constants.kt
@@ -0,0 +1,3 @@
+package dev.jakubzika.befair
+
+const val SERVER_PORT = 8080
