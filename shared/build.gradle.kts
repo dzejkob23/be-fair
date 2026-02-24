bd97f5219270f148ee8771b87ed0179bf554acce
Jakub Zíka <info@jakubzika.dev>
2026-02-24 15:48:24 +0100

    Added Ktor and Coroutines dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -22,10 +22,21 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-            // put your Multiplatform dependencies here
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.ktor.client.core)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+            implementation(libs.kotlinx.coroutines.test)
+        }
+        androidMain.dependencies {
+            implementation(libs.ktor.client.okhttp)
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+        }
+        jvmMain.dependencies {
+            implementation(libs.ktor.client.cio)
         }
     }
 }
