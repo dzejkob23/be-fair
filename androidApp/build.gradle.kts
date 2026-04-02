6b772a3773a4d98e1908fd6eed58a5984927c017
Jakub Zíka <info@jakubzika.dev>
2026-04-02 14:13:46 +0200

    Added Room library into the project

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -34,5 +34,7 @@ android {
 dependencies {
     implementation(projects.composeApp)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
     debugImplementation(compose.uiTooling)
 }
